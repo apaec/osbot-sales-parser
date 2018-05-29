09e5cef06cdb72006204d98138d95ea93eece851
Apaec <apaeccc@gmail.com>
2018-05-29 16:33:00 +0100

    Implemented salesall parsing

--- parse.py
+++ parse.py
@@ -1,35 +1,73 @@
 import os
-from bs4 import BeautifulSoup
+import sys
 import matplotlib.pyplot as plt
+from dateutil.parser import parse
+from bs4 import BeautifulSoup
 
-rel_summary_path = 'data/summary.html'
-abs_summary_path = open(os.path.join(os.path.dirname(__file__), rel_summary_path), 'r')
+rel_summary_path  = 'data/summary.html'
+abs_summary_path  = open(os.path.join(os.path.dirname(__file__), rel_summary_path), 'r')
 
-soup = BeautifulSoup(abs_summary_path, 'html.parser')
-table = soup.find("table");
+rel_salesall_path = 'data/salesall.html'
+abs_salesall_path = open(os.path.join(os.path.dirname(__file__), rel_salesall_path), 'r')
 
-summary_data = {}
+soup_summary  = BeautifulSoup(abs_summary_path , 'html.parser')
+soup_salesall = BeautifulSoup(abs_salesall_path, 'html.parser')
 
-# Iterate through all rows, omitting header row
-for row in table.find_all('tr')[1:]:
-    script_name = row.find_all('td')[1].string
-    script_total_profit = float(row.find_all('td')[2].string)
+summary_data  = {}
+salesall_data = {}
+
+# Parse summary
+summary_table = soup_summary.find("table");
+
+# Iterate through all summary rows, omitting header row
+for row in summary_table.find_all('tr')[1:]:
+    row_data = row.find_all('td')
+    script_name = row_data[1].string
+    script_total_profit = float(row_data[2].string)
 
     # Filter out the totals row
     if ":" not in script_name:
         summary_data[script_name] = script_total_profit
 
-plt.style.use('seaborn')
-plt.bar(range(len(summary_data)), summary_data.values(), align='center')
-plt.xticks(range(len(summary_data)), summary_data.keys())
+# Parse salesall
+for table in soup_salesall.find_all("table"):
+    for row in table.find_all('tr')[1:]:
+        row_data = row.find_all('td')
 
-# Prevent labels overlapping
-plt.gcf().autofmt_xdate()
-plt.show()
+        # Filter out the totals row
+        if ":" not in row_data[0].string:
 
-patches, texts, atexts = plt.pie(summary_data.values(), labels=summary_data.keys(), autopct='%1.1f%%')
-plt.legend(patches, summary_data.keys())
-plt.tight_layout()
-plt.axis('equal')
-plt.show()
-print plt.style.available
+            # Retrieve data from table
+            script_name = row_data[2].string.replace('Renewal: ', '')
+            est_profit  = float(row_data[3].string)
+            sale_date   = parse(row_data[4].string)
+
+            # Initialise dictionary entry if needed
+            if script_name not in salesall_data:
+                salesall_data[script_name] = []
+
+            # Add row sales data to dictionary
+            salesall_data[script_name].append((sale_date, est_profit))
+
+def plot_summary():
+    # Plot bar chart
+    plt.style.use('seaborn')
+    plt.bar(range(len(summary_data)), summary_data.values(), align='center')
+    plt.xticks(range(len(summary_data)), summary_data.keys())
+
+    # Prevent labels overlapping
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+    # Plot pie chart
+    patches, texts, atexts = plt.pie(summary_data.values(), labels=summary_data.keys(), autopct='%1.1f%%')
+    plt.legend(patches, summary_data.keys())
+    plt.tight_layout()
+    plt.axis('equal')
+    plt.show()
+
+def plot_salesall():
+    pass
+
+# plot_summary()
+plot_salesall()
